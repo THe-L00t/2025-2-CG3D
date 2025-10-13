dee256066fbae960f109c82828598c51a39c4d0c
THe-L00t(EPC) <sigun2001@naver.com>
2025-10-13 15:12:15 +0900

    [15]명령어 준비

--- CG1-15.cpp
+++ CG1-15.cpp
@@ -103,22 +103,28 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 	case '4':
 
 		break;
-	case 'l':
+	case '5':
+
+		break;
+	case '6':
+
+		break;
+	case '7':
+
+		break;
+	case '8':
+
+		break;
+	case '9':
+
+		break;
+	case '0':
+
+		break;
+	case 'c':
 
 		break;
 	case 't':
-
-		break;
-	case 'r':
-
-		break;
-	case 'p':
-
-		break;
-	case 'a':
-
-		break;
-	case 's':
 
 		break;
 	case 'q':
