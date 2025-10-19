733b5e636454dce06a8663cdb7819db4494199b3
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-10-19 22:37:40 +0900

    [16] init함수 구성

--- CG1-16.cpp
+++ CG1-16.cpp
@@ -36,7 +36,7 @@ int ci = colorUid(dre);
 class Object {
 public:
 	virtual void draw() = 0;
-	void init()
+	virtual void init()
 	{
 		pos = { 0.f,0.f,0.f };
 		ang = { 0.f,0.f,0.f };
@@ -55,13 +55,18 @@ protected:
 class CubeO : public Object { 
 	CubeO()
 	{
+		init();
+	}
+	virtual void init() override
+	{
+		Vertex.clear();
 		for (size_t p = 0; p < 6; p++)
 		{
 			ci = colorUid(dre);
 			for (size_t i = 0; i < 6; i++)
 			{
 				Vertex.push_back(cube[p][3 * i]);
-				Vertex.push_back(cube[p][3 * i + 1]); 
+				Vertex.push_back(cube[p][3 * i + 1]);
 				Vertex.push_back(cube[p][3 * i + 2]);
 
 				for (size_t j = 0; j < 4; j++)
@@ -70,17 +75,61 @@ class CubeO : public Object {
 				}
 			}
 		}
-		
 	}
 	virtual void draw() final
 	{
+		glm::mat4 worldTransMatrix(1.0f);
+		glm::mat4 viewTransMatrix(1.0f);
+		glm::mat4 projectTransMatrix(1.0f);
 
+		GLuint shader = bs;
+		glUseProgram(shader);
+
+		int worldTLoc = glGetUniformLocation(shader, "worldT");
+		int viewTLoc = glGetUniformLocation(shader, "viewT");
+		int projectTLoc = glGetUniformLocation(shader, "projectionT");
+
+		worldTransMatrix = glm::rotate(worldTransMatrix, glm::radians(ang.x), glm::vec3(1., 0., 0.));
+		worldTransMatrix = glm::rotate(worldTransMatrix, glm::radians(ang.y), glm::vec3(0., 1., 0.));
+		worldTransMatrix = glm::rotate(worldTransMatrix, glm::radians(30.f), glm::vec3(1., 0., 0.));
+		worldTransMatrix = glm::rotate(worldTransMatrix, glm::radians(-30.f), glm::vec3(0., 1., 0.));
+		projectTransMatrix = glm::ortho(-1.f, 1.f, -1.f, 1.f, -1.f, 1.f);
+
+
+
+		glUniformMatrix4fv(worldTLoc, 1, GL_FALSE, glm::value_ptr(worldTransMatrix));
+		glUniformMatrix4fv(viewTLoc, 1, GL_FALSE, glm::value_ptr(viewTransMatrix));
+		glUniformMatrix4fv(projectTLoc, 1, GL_FALSE, glm::value_ptr(projectTransMatrix));
+
+		int aPosition = glGetAttribLocation(shader, "a_Pos");
+		int aColor = glGetAttribLocation(shader, "a_Color");
+
+		glEnableVertexAttribArray(aPosition);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glVertexAttribPointer(aPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, 0);
+
+		glEnableVertexAttribArray(aColor);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glVertexAttribPointer(aColor, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (GLvoid*)(sizeof(float) * 3));
+
+		glDrawArrays(GL_TRIANGLES, 0, Vertex.size() / 7);
+
+		glDisableVertexAttribArray(aPosition);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		glutSwapBuffers();
 	}
 };
 
 class PyramidO : public Object{
 	PyramidO()
 	{
+		init();
+	}
+	virtual void init() override
+	{
+		Vertex.clear();
 		for (size_t p = 0; p < 5; p++)
 		{
 			ci = colorUid(dre);
@@ -99,7 +148,47 @@ class PyramidO : public Object{
 	}
 	virtual void draw() final
 	{
+		glm::mat4 worldTransMatrix(1.0f);
+		glm::mat4 viewTransMatrix(1.0f);
+		glm::mat4 projectTransMatrix(1.0f);
 
+		GLuint shader = bs;
+		glUseProgram(shader);
+
+		int worldTLoc = glGetUniformLocation(shader, "worldT");
+		int viewTLoc = glGetUniformLocation(shader, "viewT");
+		int projectTLoc = glGetUniformLocation(shader, "projectionT");
+
+		worldTransMatrix = glm::rotate(worldTransMatrix, glm::radians(ang.x), glm::vec3(1., 0., 0.));
+		worldTransMatrix = glm::rotate(worldTransMatrix, glm::radians(ang.y), glm::vec3(0., 1., 0.));
+		worldTransMatrix = glm::rotate(worldTransMatrix, glm::radians(30.f), glm::vec3(1., 0., 0.));
+		worldTransMatrix = glm::rotate(worldTransMatrix, glm::radians(-30.f), glm::vec3(0., 1., 0.));
+		projectTransMatrix = glm::ortho(-1.f, 1.f, -1.f, 1.f, -1.f, 1.f);
+
+
+
+		glUniformMatrix4fv(worldTLoc, 1, GL_FALSE, glm::value_ptr(worldTransMatrix));
+		glUniformMatrix4fv(viewTLoc, 1, GL_FALSE, glm::value_ptr(viewTransMatrix));
+		glUniformMatrix4fv(projectTLoc, 1, GL_FALSE, glm::value_ptr(projectTransMatrix));
+
+		int aPosition = glGetAttribLocation(shader, "a_Pos");
+		int aColor = glGetAttribLocation(shader, "a_Color");
+
+		glEnableVertexAttribArray(aPosition);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glVertexAttribPointer(aPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, 0);
+
+		glEnableVertexAttribArray(aColor);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glVertexAttribPointer(aColor, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (GLvoid*)(sizeof(float) * 3));
+
+		glDrawArrays(GL_TRIANGLES, 0, Vertex.size() / 7);
+
+		glDisableVertexAttribArray(aPosition);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		glutSwapBuffers();
 	}
 };
 
